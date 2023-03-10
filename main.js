019a8f1a2841b3177f780e6cc350b1700e1e945f
TirthM89 <98802520+TirthM89@users.noreply.github.com>
2023-03-10 18:06:34 +1030

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+var music = "";
+status = "";
+objects = [];
+
+function preload() {
+    music = loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+
+function start() {
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects" ;  
+    console.log("START is Started");  
+}
+
+function modelLoaded() {
+    console.log("model is loaded");
+    status = true;
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+        console.log(results);
+        objects = results;
+
+}
+
+function draw() {
+    image(video,0,0,380,380);
+    if (status != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        for (let i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Object Detected";
+            document.getElementById("noo").innerHTML = "Number of objects detected are "+objects.length;
+            fill(r,g,b);
+            percent = Math.floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent +"%",objects[i].x+4,objects[i].y+12);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);      
+            if (objects[i].label != "person") {
+                music.play();
+            }     
+        }
+    }
+
+}
+
+
